1457db9c9c5a0dfa5b4a9168fdfffc2c8fb4c81e
KovriginLeonid <162749904+KovriginLeonid@users.noreply.github.com>
2024-06-11 18:48:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ rainbowarr.forEach((char) => {
     let rainbowspan = `<span class="rainbow-char">${char}</span>`;
     rainbow.insertAdjacentHTML("beforeend", rainbowspan)
 });
+
+// modal
+const modal = document.querySelector(".modal__body");
+const openModal = document.querySelector(".modal__open");
+const closeModal = document.querySelector(".modal__close");
+
+openModal.addEventListener ('click', () => {
+    modal.showModal();
+})
+closeModal.addEventListener ('click', () => {
+    modal.close();
+})
